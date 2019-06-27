82cfb5720fa1168ec5d08aa05796e95cde09ace8
oleg <olegkr@wizardsdev.com>
2019-06-27 16:55:19 +0300

    regenerate docks

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module IdeaboardApi
+module TokenAuthApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -37,5 +37,11 @@ module IdeaboardApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+  end
+end
+
+unless Rails.env.production?
+  RSpec.configure do |config|
+    config.swagger_dry_run = false
   end
 end
