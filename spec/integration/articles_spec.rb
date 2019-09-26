38c152683de77bb9a4bb8eee79daf07a4943c649
oleg <krasno1986@gmail.com>
2019-09-26 13:20:29 +0300

    fix docks

--- spec/integration/articles_spec.rb
+++ spec/integration/articles_spec.rb
@@ -33,7 +33,7 @@ RSpec.describe 'articles' do
 
       let(:signed_in_user) { create :user}
 
-      response '200', 'group created' do
+      response '200', 'Articles founded' do
         before { create_list :article, 2, user: signed_in_user }
         run_test!
       end
@@ -44,17 +44,17 @@ RSpec.describe 'articles' do
     let(:signed_in_user) { create :user }
     let(:article)           { create :article, user: signed_in_user }
 
-    get 'Show a article' do
+    get 'Show article' do
       tags 'Articles'
       consumes 'application/json'
       parameter name: :id, in: :path, type: :string
 
-      response '200', 'Articles founded' do
+      response '200', 'Article found' do
         let(:id) { article.id }
         run_test!
       end
 
-      response '404', 'Articles not found' do
+      response '404', 'Article not found' do
         let(:id) { 'invalid' }
         run_test!
       end
@@ -63,9 +63,9 @@ RSpec.describe 'articles' do
 
   path '/api/v1/articles/{id}' do
     let(:signed_in_user) { create :user }
-    let(:article)           { create :article, user: signed_in_user }
+    let(:article)        { create :article, user: signed_in_user }
 
-    delete 'Delete a article' do
+    delete 'Delete article' do
       tags 'Articles'
       consumes 'application/json'
       parameter name: :id, in: :path, type: :string
@@ -81,7 +81,7 @@ RSpec.describe 'articles' do
     let(:signed_in_user) { create :user }
     let(:article)           { create :article, user: signed_in_user }
 
-    put 'Update a article' do
+    put 'Update article' do
       tags 'Articles'
       consumes 'application/json'
       parameter name: :id, in: :path, type: :string
