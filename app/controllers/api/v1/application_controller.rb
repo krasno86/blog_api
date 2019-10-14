d2bf741351b508feefcb70a8005742d854b9c8a3
oleg <krasno1986@gmail.com>
2019-10-14 12:11:02 +0300

    remove mess

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -6,12 +6,7 @@ class Api::V1::ApplicationController < ActionController::API
   respond_to :json
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :skip_session
-
-  def skip_session
-    request.session_options[:skip] = true
-  end
-
+  
   protected
 
   def configure_permitted_parameters
